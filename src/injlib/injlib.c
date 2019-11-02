ac5e20aef687cb096ff370be32e02d03e993e504
Georgiy0 <kubrin.eg.s@gmail.com>
2019-11-02 18:25:00 +0000

    Added communication with AVCore (injdrv asks InjectPlugin weather the APC should be injected in the new process).

--- src/injlib/injlib.c
+++ src/injlib/injlib.c
@@ -3,6 +3,10 @@
 #include <ntddk.h>
 #include <ntimage.h>
 
+#include "KMUMcomm.h"
+#include "EventsKMStructures.h"
+#include "KMEventsAPI.h"
+
 #if defined(_M_AMD64) || defined(_M_ARM64)
 # define INJ_CONFIG_SUPPORTS_WOW64
 #endif
@@ -1286,7 +1290,66 @@ InjCreateProcessNotifyRoutineEx(
 
   if (CreateInfo)
   {
-    InjCreateInjectionInfo(NULL, ProcessId);
+    if (!AVCommIsInitialized() || AVCommIsExcludedPID(CreateInfo->CreatingThreadId.UniqueProcess))
+    {
+      return;
+    }
+
+    AV_EVENT_PROCESS_CREATE eventProcessCreate = { 0 };
+    eventProcessCreate.PID = (int)(__int64)ProcessId;
+    eventProcessCreate.parentPID = (int)(__int64)CreateInfo->ParentProcessId;
+    eventProcessCreate.creatingPID = (int)(__int64)CreateInfo->CreatingThreadId.UniqueProcess;
+    eventProcessCreate.creatingTID = (int)(__int64)CreateInfo->CreatingThreadId.UniqueThread;
+
+    SIZE_T umBuffCommandLineSize, umBuffFileNameSize;
+    NTSTATUS status = STATUS_SUCCESS;
+    if (CreateInfo->FileOpenNameAvailable && CreateInfo->ImageFileName)
+    {
+      // Put file name info to UM buffer
+      eventProcessCreate.imageFileNameSize = CreateInfo->ImageFileName->Length;
+      status = AVCommCreateBuffer(CreateInfo->ImageFileName->Buffer, eventProcessCreate.imageFileNameSize, &eventProcessCreate.imageFileName, &umBuffFileNameSize);
+      if (status != STATUS_SUCCESS)
+      {
+        // couldn't allocate memory in UM
+        return;
+      }
+
+      // Put command line info to UM buffer
+      eventProcessCreate.commandLineSize = CreateInfo->CommandLine->Length;
+      status = AVCommCreateBuffer(CreateInfo->CommandLine->Buffer, eventProcessCreate.commandLineSize, &eventProcessCreate.commandLine, &umBuffCommandLineSize);
+      if (status != STATUS_SUCCESS)
+      {
+        // couldn't allocate memory in UM
+        return;
+      }
+    }
+
+    AV_EVENT_RESPONSE UMResponse;
+    ULONG replyLength = sizeof(AV_EVENT_RESPONSE);
+
+    // Send event to the AVCore UM service and wait for the response
+    status = AVCommSendEvent(AvProcessCreate,
+      &eventProcessCreate,
+      sizeof(AV_EVENT_PROCESS_CREATE),
+      &UMResponse,
+      &replyLength);
+
+    if (CreateInfo->FileOpenNameAvailable && CreateInfo->ImageFileName)
+    {
+      // free memory in UM
+      NTSTATUS freeStatus = AVCommFreeBuffer(&eventProcessCreate.imageFileName, &umBuffFileNameSize);
+      if (freeStatus != STATUS_SUCCESS) { return; }
+      freeStatus = AVCommFreeBuffer(&eventProcessCreate.commandLine, &umBuffCommandLineSize);
+      if (freeStatus != STATUS_SUCCESS) { return; }
+    }
+
+    if (status == STATUS_SUCCESS) // check whether communication with UM was successfull.
+    {
+      if (UMResponse.Status == AvEventStatusAllow)
+      {
+        InjCreateInjectionInfo(NULL, ProcessId);
+      }
+    }
   }
   else
   {
